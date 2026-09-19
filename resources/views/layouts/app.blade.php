a0e0a8d0139b46ca62875e46b028c4a528558b05
jesuraja.i <jesuraja.i@siamcmputing.com>
2026-09-19 17:56:06 +0530

    ui design changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,9 +10,9 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   @production
-  <link rel="stylesheet" href="{{ url('/public/css/shrine-theme.css') }}?v=5">
+  <link rel="stylesheet" href="{{ url('/public/css/shrine-theme.css') }}?v=6">
   @else
-  <link rel="stylesheet" href="{{ asset('css/shrine-theme.css') }}?v=5">
+  <link rel="stylesheet" href="{{ asset('css/shrine-theme.css') }}?v=6">
   @endproduction
   @yield('styles')
 </head>
